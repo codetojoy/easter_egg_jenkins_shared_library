a8570ec3917824d055ad57154656f60d9c5d48a9
Michael Easter <codetojoy@gmail.com>
2018-06-13 20:45:58 -0300

    incremental change as part of a larger frenzy of activity

--- src/net/codetojoy/shared/Bar.groovy
+++ src/net/codetojoy/shared/Bar.groovy
@@ -2,6 +2,9 @@
 package net.codetojoy.shared
 
 class Bar {
+    static def G1 = "G1"
+    static def G2 = "G2"
+
     def id
 
     def emitLog() { 
--- vars/folderFoo.groovy
+++ vars/folderFoo.groovy
@@ -3,4 +3,6 @@ def emitLog(message) {
     println "TRACER folderFoo. message: ${message}"
     def bar = new net.codetojoy.shared.Bar(id: 5150)
     bar.emitLog()
+
+    println "TRACER test : " + net.codetojoy.shared.Bar.G1
 }
