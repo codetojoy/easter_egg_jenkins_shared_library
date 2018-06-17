4e0039a9fafc8bd08f0900eb03612c00b4da375e
Michael Easter <codetojoy@gmail.com>
2018-06-17 13:22:15 -0300

    incremental change as part of a larger frenzy of activity

--- vars/folderFoo.groovy
+++ vars/folderFoo.groovy
@@ -1,8 +1,13 @@
 
+class Constants {
+    static def MY_GLOBAL_VAR = "mozart"
+}
+
 def emitLog(message) {
     println "TRACER folderFoo. message: ${message}"
     def bar = new net.codetojoy.shared.Bar(id: 5150)
     bar.emitLog()
 
     println "TRACER test : " + net.codetojoy.shared.Bar.G1
+    println "TRACER t2 : " + Constants.MY_GLOBAL_VAR
 }
